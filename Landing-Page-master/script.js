a1280d8895c386a7651b1714bb112447263a17f7
ahmadamjada <165276048+ahmadamjada@users.noreply.github.com>
2024-10-09 20:22:57 +0500

    update the tasty resturant link add new website

--- Landing-Page-master/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const sectionToAnimate = document.querySelector('.section-to-animate');
-    const sectionTop = sectionToAnimate.offsetTop;
-    const windowHeight = window.innerHeight;
-  
-    if (window.scrollY >= sectionTop - windowHeight / 2) {
-      sectionToAnimate.classList.add('animate-show');
-    }
-  });
